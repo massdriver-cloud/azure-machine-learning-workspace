0e1bf882c37610855c1e1ee2f6e52994c5d0c3d0
mclacore <michael@massdriver.cloud>
2023-03-15 17:02:45 -0600

    Adding tags

--- src/iam.tf
+++ src/iam.tf
@@ -2,6 +2,7 @@ resource "azurerm_user_assigned_identity" "main" {
   name                = var.md_metadata.name_prefix
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+  tags                = var.md_metadata.default_tags
 }
 
 resource "azurerm_role_assignment" "storage_account_data" {
--- src/main.tf
+++ src/main.tf
@@ -8,6 +8,7 @@ locals {
 resource "azurerm_resource_group" "main" {
   name     = var.md_metadata.name_prefix
   location = var.workspace.location
+  tags     = var.md_metadata.default_tags
 }
 
 resource "azurerm_machine_learning_workspace" "main" {
